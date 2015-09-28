fe8b5821c41b12f0fbffe8984ce61fd7ed018dde
juliakvashyna <julia.kvashina@gmail.com>
2015-09-28 23:07:43 +0300

    add volley

---
+++ app/src/main/java/com/assigment/trelloplus/api/TrelloUrls.java
@@ -0,0 +1,12 @@
+package com.assigment.trelloplus.api;
+
+/**
+ * Created by Juli on 28.09.2015.
+ */
+public interface TrelloUrls {
+    String API_URL = "https://trello.com/1/";
+    String API_KEY = "0c2dfd4d3c851ff24fa99be5f57c8255";
+    String SECRET_KEY = "782f7d9f9f7263eb4d5d475e9ab059d5e7cad9e62d66b7be59b46a3195b74f7";
+    String AUTHORIZE = API_URL + "authorize";
+
+}
---
+++ app/src/main/java/com/assigment/trelloplus/view/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.assigment.trelloplus.view;
+
+import android.app.Activity;
+
+/**
+ * Created by Juli on 27.09.2015.
+ */
+public class BaseActivity extends Activity {
+}
